32c552fa1c6b8244e4a60517bc80053d0dc912da
Ayoade1st <fissyayoade@gmail.com>
2024-09-19 15:28:03 +0100

    Updated

--- changes.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.remove('C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python37')
-sys.path.append('C:\\Users\\USER\\Pictures\Camera Roll\\Financial-Portfolio-Flask-master\\venv\\lib\\python2.7')
-sys.path[0]= r'C:\Users\USER\Pictures\Camera Roll\Financial-Portfolio-Flask-master\venv\bin'
-os.environ['PATH']=r'C:\Users\USER\Pictures\Camera Roll\Financial-Portfolio-Flask-master\venv\bin'
-
-
-
